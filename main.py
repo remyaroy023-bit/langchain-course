de50e00d9e71487173fb19b72acb15caca05861a
Remya Roy <remyaroy023@gmail.com>
2026-07-03 09:48:47 +0530

    Add LangSmith tracing

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import os
-
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-
+from langchain_core.prompts import PromptTemplate
 load_dotenv()
 
 
 def main():
-    print("Hello from langchain!")
+    information = """
+The cat (Felis catus), also called domestic cat and house cat, is a small domesticated carnivorous mammal. It is a member of Felidae, the family of mammals in the order Carnivora also colloquially referred to as cats. It is an obligate carnivore, requiring a predominantly meat-based diet. Its retractable claws are adapted to killing small prey species such as mice and rats. It has a strong, flexible body, quick reflexes, and sharp teeth, and its night vision and sense of smell are well developed. It is a social species, but a solitary hunter and a crepuscular predator. Cat communication includes meowing, purring, trilling, hissing, growling, grunting, and body language. It can hear sounds too faint or too high in frequency for human ears, such as those made by small mammals. It secretes and perceives pheromones. Cat intelligence is evident in its ability to adapt, learn through observation, and solve problems. The domestic cat is the only domesticated species of the family Felidae.
 
+Advances in archaeology and genetics have shown that the domestication of the cat started in the Near East around 7500 BCE. Today, the domestic cat occurs across the globe and is valued by humans for companionship and its ability to kill vermin. It is commonly kept as a pet, working cat, and pedigreed cat shown at cat fancy events. Out of the estimated 600 million domestic cats worldwide, 400 million reside in Asia, including 58 million in China. About 73.8 million cats are estimated to live in the United States, and about 10.9 million cats in the United Kingdom. It also ranges freely as a feral cat, avoiding human contact. Pet abandonment contributes to increasing of the global feral cat population, which has driven the decline of bird, mammal, and reptile species. Population control includes spaying and neutering.
+"""
 
+    summary_template="""
+    given the {information} write a short brief summary in simple terms what a cat is.
+     #use one sentence only
+     #in simple terms
+    """
+    summary_prompt_template = PromptTemplate(input_variables=["information"],template=summary_template)
+    llm = ChatOpenAI(
+        model="openrouter/free",  # free-tier model (often available)
+        api_key=os.getenv("OPENROUTER_API_KEY"),
+        base_url="https://openrouter.ai/api/v1",
+        temperature=0.7
+    )
+
+    chain = summary_prompt_template|llm
+    response = chain.invoke(input={"information":information})
+    print(response.content)
 if __name__ == "__main__":
     main()
